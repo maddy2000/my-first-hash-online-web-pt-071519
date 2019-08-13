c24e5cf98d761beae8e2627a62493d7d2bd2b660
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-13 02:56:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,17 @@
-def my_hash
-    your_hash = {"name"=> "madelyn", "race"=> "african"}
-    your_hash ["name"]
-end
+ 1) Challenge I: Instantiating Hashes #my_hash uses the literal constructor tocreate a hash that contains key/value pairs
+     Failure/Error: expect(my_hash).to be_a(Hash)
+       expected "madelyn" to be a kind of Hash
+     # ./spec/my_first_hash_spec.rb:8:in `block (3 levels) in <top (required)>'
 
+Finished in 0.01274 seconds (files took 0.10895 seconds to load)
+1 example, 1 failure
+
+Failed examples:
+
+rspec ./spec/my_first_hash_spec.rb:7 # Challenge I: Instantiating Hashes #my_hash uses the literal constructor to create a hash that contains key/value pairs
+
+[02:52:40] (master) my-first-hash-online-web-pt-071519
+// ♥
 
 
 
